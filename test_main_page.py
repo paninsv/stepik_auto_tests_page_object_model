45d054902f227d34e16c3956a6c2a99d9bf49a65
s.panin <s.panin1976@mail.ru>
2022-04-15 15:59:18 +0200

    add conftest

---
+++ test_main_page.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    # login_link = browser.find_element_by_css_selector("#login_link")
+    login_link = browser.find_element(by=By.CSS_SELECTOR, value="#login_link")
+    login_link.click()
